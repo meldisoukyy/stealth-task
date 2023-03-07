963470c49795c156661b006e5270aafae70221e3
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-03-07 20:53:37 +0200

    build: move fancy-log and morgan to dependancies

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const process = require('process');
 const { logger } = require('./logger.js');
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development')
-  app.use(logger());
+app.use(logger());
 
 app.get('/', (req, res) => {
   res.send('Hello, Friend!');
