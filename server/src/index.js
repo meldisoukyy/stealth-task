fa10dca74f0893aafe917878afe71b2f2587e8b2
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-03-12 17:14:32 +0200

    style: update require lines style

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,17 @@
+require('dotenv').config();
+
+const process = require('process');
+
 const express = require('express');
+
 const { logger } = require('./logger.js');
 const { v1Routes } = require('./v1/routes/index.js');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(logger());
 app.use('/v1', v1Routes);
 
-app.listen(3000);
+app.listen(PORT);
--- server/src/v1/routes/index.js
+++ server/src/v1/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const { tasksRoutes } = require('./tasksRoutes');
 const router = express.Router();
 
