3e79a9363f0813cad194ce5009655cfd9c51bab8
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-03-15 09:06:33 +0200

    chore: add `db` object to models index

--- server/src/v1/models/index.js
+++ server/src/v1/models/index.js
@@ -4,6 +4,7 @@ const { Sequelize } = require('sequelize');
 
 const config = require('../config/config.json');
 
+const db = {};
 let dbConfig;
 
 if (process.env.NODE_ENV === 'development') {
@@ -18,12 +19,13 @@ const sequelize = new Sequelize({
 const testConnection = async () => {
   try {
     await sequelize.authenticate();
-    console.log('Database connected successfully!');
+    console.log('Database is connected successfully!');
   } catch (err) {
-    console.error('Error: Can\'t establish the connection to the database!');
+    throw new Error('Can\'t establish the connection to the database!');
   }
 };
 
-testConnection();
-
-module.exports = sequelize;
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+db.testConnection = testConnection;
+module.exports = db;
