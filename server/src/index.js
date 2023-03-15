ca0ed63ecf1c3dce04ca9cd4a0bd56dc2100ff20
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-03-15 09:05:10 +0200

    chore: add check of the database connection before establishing the server

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,16 @@
 const process = require('process');
 
 const app = require('./app');
+const db = require('./v1/models');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT);
+(async () => {
+  try {
+    await db.testConnection();
+    app.listen(PORT);
+    console.log('Server is connected successfully!');
+  } catch (err) {
+    console.error('Error: Can\'t establish the connection to the server!');
+  }
+})();
